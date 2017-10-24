4be66148c6dc711debafdcc2baf83649ccff21eb
jdominguezmartinan <jdominguezmartinan@victoria1.danielcastelao.org>
2017-10-24 09:35:47 +0200

    commit1

---
+++ src/boletin3_1/boletin3_1.java
@@ -0,0 +1,23 @@
+package boletin3_1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jdominguezmartinan
+ */
+public class boletin3_1 {
+    
+    
+    
+ public void main (String []args){
+     
+     
+     
+ }   
+    
+}
